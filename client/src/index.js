012b7342a71753b1f74bbf463b72ba08edff0f89
gaut-b <gautier.bayle@gmail.com>
2020-10-15 14:19:07 +0200

    Create Dockerfile and change add client to docker-compose file

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import { HashRouter } from 'react-router-dom';
 
 import { store } from './redux/store';
 
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
